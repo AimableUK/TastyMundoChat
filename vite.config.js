baf730c2564e0dd49b67527857c29959322bafa1
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-06-20 07:35:28 +0200

    fixed the large sized files checking by using the rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer({ open: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("mui")) return "mui";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
