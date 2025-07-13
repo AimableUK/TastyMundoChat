33a807046da3ee941937526dc08df789dc1de863
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-13 19:58:54 +0200

    Cleaning Up Unneeded Codes

--- src/App/AICHAT/AIRecipe/AIRecipe.jsx
+++ src/App/AICHAT/AIRecipe/AIRecipe.jsx
@@ -7,7 +7,7 @@ function AIRecipe({recipe}) {
         className="-mt-56 mb-20 text-gray-200"
         aria-live="polite"
       >
-        <h2>Chef Claude Recommends: </h2>
+        <h2>Tasty Mundo Recommends: </h2>
         <ReactMarkdown>{recipe}</ReactMarkdown>
       </section>
     </div>
